c847944cc12b1664f47283b6afabf492b06697fe
tomo <kaoru.nish@gmail.com>
2018-07-06 17:52:42 +0900

    first commit

---
+++ src/takesi/analytics.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace takesi;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\plugin\PluginBase;
+use takesi\analyticsTask;
+
+class analytics extends PluginBase implements Listener
+{
+
+    public $db;
+    public $pair = array();
+
+    public function onEnable()
+    {
+        $this->getLogger()->notice("Loaded!");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        if (!file_exists($this->getDataFolder())) {
+            mkdir($this->getDataFolder(), 0744, true);
+            mkdir($this->getDataFolder() . "/" . "skins" . "/", 0744, true);
+        }
+        $this->db = new \SQLite3($this->getDataFolder() . "/logindata.sqlite3");
+        $this->db->exec(
+            "CREATE TABLE IF NOT EXISTS playerdata(
+			name TEXT NOT NULL PRIMARY KEY,
+			date TEXT NOT NULL,
+			time TEXT NOT NULL
+			)"
+        );
+        $this->getServer()->getScheduler()->scheduleRepeatingTask(new analyticsTask($this), 20);
+    }
+
+    public function isExists($name, $date)
+    {
+        $que = $this->db->prepare("SELECT * FROM playerdata WHERE name = :name AND date= :now");
+        $que->bindValue(":name", $name, SQLITE3_TEXT);
+        $que->bindValue(":now", $date, SQLITE3_TEXT);
+        $data = $que->execute();
+        $result = [];
+        while ($d = $data->fetchArray()) {
+            $result[] = $d;
+        }
+        return count($result) > 0;
+    }
+
+    public function createPlayerdata($name, $date, $long)
+    {
+        $this->db->exec("INSERT INTO playerdata VALUES(\"$name\", $date, $long)");
+    }
+
+    public function updatePlayerdata($name, $date, $long)
+    {
+        $this->db->exec("UPDATE playerdata SET time = " . $long . " WHERE name = \"$name\" AND date= " . $date . "");
+    }
+
+    public function getPlayerdata($name, $date)
+    {
+        $que = $this->db->prepare("SELECT * FROM playerdata WHERE name = :name AND date= :now");
+        $que->bindValue(":name", $name, SQLITE3_TEXT);
+        $que->bindValue(":now", $date, SQLITE3_TEXT);
+        $data = $que->execute();
+        $result;
+        while ($d = $data->fetchArray()) {
+            $result = $d;
+        }
+        return $result;
+    }
+
+    public function onJoin(PlayerJoinEvent $event)
+    {
+        $player = $event->getPlayer();
+        $this->pair[$player->getName()] = time();
+        if (extension_loaded("gd")) {
+            $img = imagecreatetruecolor(64, 32);
+            $colors = [];
+            $bytes = $player->getSkin()->getSkinData();
+            $x = $y = $c = 0;
+            while ($y < 32) {
+                $cid = substr($bytes, $c, 3);
+                if (!isset($colors[$cid])) {
+                    $colors[$cid] = imagecolorallocate($img, ord($cid{0}), ord($cid{1}), ord($cid{2}));
+                }
+                imagesetpixel($img, $x, $y, $colors[$cid]);
+                $x++;
+                $c += 4;
+                if ($x === 64) {
+                    $x = 0;
+                    $y++;
+                }
+            }
+            imagepng($img, $this->getDataFolder() . "/skins/" . $player->getName() . ".png");
+            imagedestroy($img);
+        } else {
+            $player->sendMessage("Extension is not yet loaded!");
+        }
+
+    }
+
+    public function onQuit(PlayerQuitEvent $event)
+    {
+        var_dump($this->pair);
+        $jointime = $this->pair[$event->getPlayer()->getName()];
+        $difftime = time() - $jointime;
+        $this->getLogger()->info($event->getPlayer()->getName() . " stayed in this server for " . $difftime . "s");
+        if ($this->isExists($event->getPlayer()->getName(), date("Ymd"))) {
+            $data = $this->getPlayerdata($event->getPlayer()->getName(), date("Ymd"));
+            var_dump($data);
+            $this->updatePlayerdata($event->getPlayer()->getName(), $data["date"], (((int) $data["time"]) + ((int) $difftime)), date("Ymd"));
+            $this->getLogger()->info("So he or she stayed all in this server for " . (((int) $data["time"]) + ((int) $difftime)) . "s today.");
+        } else {
+            $this->createPlayerdata($event->getPlayer()->getName(), date("Ymd"), $difftime);
+        }
+        unset($this->pair[$event->getPlayer()->getName()]);
+    }
+
+}
---
+++ src/takesi/analyticsTask.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace takesi;
+
+use pocketmine\scheduler\PluginTask;
+use pocketmine\Server;
+use takesi\analytics;
+
+class AnalyticsTask extends PluginTask
+{
+
+    public $plugin;
+
+    public $ran = true;
+
+    public function __construct(analytics $plugin)
+    {
+        parent::__construct($plugin);
+        $this->plugin = $plugin;
+    }
+
+    public function getPlugin()
+    {
+        return $this->plugin;
+    }
+
+    public function onRun($tick)
+    {
+        //1530802800 2018/07/06 00:00:00
+        //1530889200 2018/07/07 00:00:00
+        //So 1 day =86400s
+        if (time() % 1530866100 == 0) {
+            $players = Server::getInstance()->getOnlinePlayers();
+            $this->plugin->getLogger()->info("changing day is now going...");
+            foreach ($players as $player) {
+                $jointime = $this->plugin->pair[$player->getName()];
+                $difftime = time() - $jointime;
+                $this->plugin->getLogger()->info($player->getName() . " stayed in this server for " . $difftime . "s");
+                if ($this->plugin->isExists($player->getName(), date("Ymd", time() - 5000))) {
+                    $data = $this->plugin->getPlayerdata($player->getName(), date("Ymd", time() - 5000));
+                    var_dump($data);
+                    $this->plugin->updatePlayerdata($player->getName(), $data["date"], (((int) $data["time"]) + ((int) $difftime)), date("Ymd", time() - 5000));
+                    $this->plugin->getLogger()->info("So he or she stayed all in this server for " . (((int) $data["time"]) + ((int) $difftime)) . "s today.");
+                } else {
+                    $this->plugin->createPlayerdata($player->getName(), date("Ymd", time() - 5000), $difftime);
+                }
+                $this->plugin->pair[$player->getName()] = time();
+            }
+        }
+
+        //$this->getPlugin()->removeTask($this->getTaskId());
+    }
+}
